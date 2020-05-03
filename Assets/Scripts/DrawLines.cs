402d9261fb51c95d29257914df691a29bc319666
Mehmet Salih Koçak <msalihkocak@Mehmets-MacBook-Pro.local>
2020-05-03 18:16:30 +0300

    Initial commit

---
+++ Assets/Scripts/DrawLines.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLines : MonoBehaviour
+{
+    Coords point = new Coords(10,20);
+    
+    Coords yAxisStartPoint = new Coords(0,100);
+    Coords yAxisEndPoint = new Coords(0,-100);
+    
+    Coords xAxisStartPoint = new Coords(-160,0);
+    Coords xAxisEndPoint = new Coords(160,0);
+    
+    
+    void Start()
+    {
+        Debug.Log(point.ToString());
+        Coords.DrawPoint(point, 2, Color.blue);
+        Coords.DrawLine(yAxisStartPoint, yAxisEndPoint, 0.4f, Color.green);
+        Coords.DrawLine(xAxisStartPoint, xAxisEndPoint, 0.4f, Color.red);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TestingScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.forward = Vector3.back;
+        GameObject.Find("Test");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
